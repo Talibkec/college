194e3bcd329404838c6fe5876b3dd51c13300bf9
Talib <talib@keck.ac.in>
2019-01-29 21:03:38 +0530

    Adding Custom JS folder

---
+++ src/main/webapp/js/homepage.js
@@ -0,0 +1,20 @@
+ $(document).ready(function(){
+ alert("Hello");
+ getUrlParameter();
+
+ });
+
+ function getUrlParameter() {
+                 var sPageURL = window.location.search.substring(1),
+                     sURLVariables = sPageURL.split('&'),
+                     sParameterName,
+                     i;
+
+                 for (i = 0; i < sURLVariables.length; i++) {
+                     sParameterName = sURLVariables[i].split('=');
+
+                     if (sParameterName[0] === sParam) {
+                         //return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                     }
+                 }
+             };

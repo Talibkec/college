8d9f706728201f1b8818c9825c3daf614137eb3d
Talib <talib@keck.ac.in>
2019-02-19 19:53:03 +0530

    Fetch notices sorted by date.

--- src/main/java/com/college/repository/NoticeBoardRepository.java
+++ src/main/java/com/college/repository/NoticeBoardRepository.java
@@ -12,21 +12,21 @@ import java.util.List;
 @Repository
 public interface NoticeBoardRepository extends JpaRepository<NoticeBoard, Long> {
 
-    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType")
+    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType order by nb.date DESC")
     public List<NoticeBoard> getCseNotices(@Param("noticeType") String noticeType);
 
-    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType")
+    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType order by nb.date DESC")
     public List<NoticeBoard> getCivilNotices(@Param("noticeType") String noticeType);
 
-    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType")
+    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType order by nb.date DESC")
     public List<NoticeBoard> getMechNotices(@Param("noticeType") String noticeType);
 
-    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType")
+    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType order by nb.date DESC")
     public List<NoticeBoard> getElecNotices(@Param("noticeType") String noticeType);
 
-    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType")
+    @Query("select nb from NoticeBoard nb where nb.noticeType = :noticeType order by nb.date DESC")
     public List<NoticeBoard> getAshNotices(@Param("noticeType") String noticeType);
 
-    @Query("select nb from NoticeBoard nb where nb.noticeType = :notice or nb.noticeType = :news")
+    @Query("select nb from NoticeBoard nb where nb.noticeType = :notice or nb.noticeType = :news order by nb.date DESC")
     List<NoticeBoard> getHomePageNotices(@Param("notice") String notice, @Param("news") String news);
 }

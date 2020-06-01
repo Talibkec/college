c9a3a6c70e3f642baee07b9f2d782658286a4d6f
Ankit Dutta <46992030+ankitdutta170@users.noreply.github.com>
2020-06-01 09:23:14 +0530

    Using Google Calendar API in Leave Management Application

---
+++ src/main/java/com/college/core/controller/firebase/CalendarService.java
@@ -0,0 +1,72 @@
+package com.college.core.controller.firebase;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Collections;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.common.io.Files;
+
+
+public class CalendarService {
+    private static final String APPLICATION_NAME = "calendar-json.googleapis.com";
+
+    /** E-mail address of the service account. */
+    private static final String SERVICE_ACCOUNT_EMAIL = "attendance-leave@attendance-278911.iam.gserviceaccount.com";
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport httpTransport;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+
+    public  Calendar configure() {
+        try {
+            try {
+                httpTransport = new NetHttpTransport();
+                // check for valid setup
+                if (SERVICE_ACCOUNT_EMAIL.startsWith("Enter ")) {
+                    System.err.println(SERVICE_ACCOUNT_EMAIL);
+                    System.exit(1);
+                }
+                URL loc = this.getClass().getResource("/attendance-278911-5f653aa76d72.p12");
+                String path = loc.getPath();
+                File file = new File(path);
+                String p12Content = Files.readFirstLine(file, Charset.defaultCharset());
+                if (p12Content.startsWith("Please")) {
+                    System.err.println(p12Content);
+                    System.exit(1);
+                }
+                // service account credential (uncomment setServiceAccountUser for domain-wide delegation)
+                GoogleCredential credential = new GoogleCredential.Builder()
+                        .setTransport(httpTransport)
+                        .setJsonFactory(JSON_FACTORY)
+                        .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+                        .setServiceAccountScopes(Collections.singleton(CalendarScopes.CALENDAR))
+                        .setServiceAccountPrivateKeyFromP12File(file)
+                        .build();
+                Calendar   client = new com.google.api.services.calendar.Calendar.Builder(
+                        httpTransport, JSON_FACTORY, credential)
+                        .setApplicationName(APPLICATION_NAME).build();
+                System.out.println("Client : "+client);
+                return client;
+
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        System.exit(1);
+        return null;
+    }
+}
--- src/main/java/com/college/core/controller/firebase/LeaveStatusMail.java
+++ src/main/java/com/college/core/controller/firebase/LeaveStatusMail.java
@@ -15,15 +15,24 @@ import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileSystemUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.CalendarListEntry;
 
 import javax.mail.Message;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeUtility;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+        
 
 import java.io.File;
 import java.io.IOException;
@@ -45,7 +54,7 @@ public class LeaveStatusMail {
     
     @ResponseBody
     @RequestMapping(value = "/rs", method = RequestMethod.GET)
-    public String LeaveStatusMailController(@RequestParam("params") String params ){
+    public String LeaveStatusMailController(@RequestParam("params") String params ) throws  IOException, ParseException{
         Gson gson= new Gson();
         LeaveStatusDetail leaveStatusDetail = gson.fromJson(params,LeaveStatusDetail.class);
         leaveId = leaveStatusDetail.getLeaveId();
@@ -58,10 +67,73 @@ public class LeaveStatusMail {
         requestDate = leaveStatusDetail.getRequestDate();
         System.out.println("Leave Status is being called with following parameters. " + params );
         File tempDirectory = new File(System.getProperty("java.io.tmpdir"));
-        String email = leaveStatusDetail.getEmail();
+        String email = "ankitdutta170@gmail.com";
         sendLeaveStatusMail(email,leaveStatusDetail);
+        
+        addEventToCalendar(leaveStatusDetail);
+        
         return "";
     }
+    @ExceptionHandler(IOException.class)
+    public void addEventToCalendar(LeaveStatusDetail leaveStatusDetail) throws IOException, ParseException
+    {
+        Event event = new Event();
+        Calendar service =null;
+
+        event.setSummary("Calendar Testing");
+        event.setLocation("India");
+        event.setDescription("Desired description");
+
+        /*ArrayList<EventAttendee> attendees = new ArrayList<EventAttendee>();
+        attendees.add(new EventAttendee().setEmail("ankitdutta170@gmail.com"));
+        // ...
+        event.setAttendees(attendees);*/
+
+
+        // set the number of days
+        String startDate = leaveStatusDetail.getStartDate();
+        String endDate = leaveStatusDetail.getEndDate();
+        String temp[];
+        String delimeter ="-";
+        temp = startDate.split(delimeter);
+        int startYear = Integer.parseInt(temp[0]) ;
+        int startMonth = Integer.parseInt(temp[1]);
+        int startDay = Integer.parseInt(temp[2]);
+        java.util.Calendar startCal = java.util.Calendar.getInstance();
+        startCal.set(java.util.Calendar.YEAR, startYear);
+        startCal.set(java.util.Calendar.MONTH, startMonth);
+        startCal.set(java.util.Calendar.DATE, startDay);
+        startCal.set(java.util.Calendar.HOUR_OF_DAY, 7);
+        startCal.set(java.util.Calendar.MINUTE, 0);
+        Date getStartDate = startCal.getTime();
+        
+        temp = endDate.split(delimeter);
+        int endYear = Integer.parseInt(temp[0]) ;
+        int endMonth = Integer.parseInt(temp[1]);
+        int endDay = Integer.parseInt(temp[2]);
+        java.util.Calendar endCal = java.util.Calendar.getInstance();
+        endCal.set(java.util.Calendar.YEAR, endYear);
+        endCal.set(java.util.Calendar.MONTH, endMonth);
+        endCal.set(java.util.Calendar.DATE, endDay);
+        endCal.set(java.util.Calendar.HOUR_OF_DAY, 18);
+        endCal.set(java.util.Calendar.MINUTE, 0);
+        Date getEndDate = startCal.getTime();
+        
+        
+        DateTime start = new DateTime(getStartDate);
+        event.setStart(new EventDateTime().setDateTime(start));
+        DateTime end = new DateTime(getEndDate);
+        event.setEnd(new EventDateTime().setDateTime(end));
+
+        service = new CalendarService().configure();
+        Event createdEvent = service.events().insert("primary", event).execute();
+
+        System.out.println("Data is :"+createdEvent.getId());
+        CalendarListEntry calendarListEntry = service.calendarList().get("calendarId").execute();
+
+        System.out.println(calendarListEntry.getSummary());
+    }
+    
     private void  sendLeaveStatusMail(String email, LeaveStatusDetail leaveStatusDetail){
         String subject = "Leave Request Status";
         String mailBody = "Sir,"+"\n\n"+"The "+leaveStatusDetail.getLeaveType()+" which you have requested on "+leaveStatusDetail.getRequestDate()+" ,between "+leaveStatusDetail.getStartDate()+" and "+leaveStatusDetail.getEndDate()+" has been "+leaveStatusDetail.getStatus();

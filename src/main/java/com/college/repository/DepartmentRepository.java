f054c565e7f80a2f03a56aa674ff0382f701b2ed
Anshuman Badal <apcbadal@gmail.com>
2020-07-06 13:51:57 +0530

    Fix bug for Department HOD Name

--- src/main/java/com/college/repository/DepartmentRepository.java
+++ src/main/java/com/college/repository/DepartmentRepository.java
@@ -3,9 +3,11 @@ package com.college.repository;
 import com.college.core.entity.Department;
 import com.college.core.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,10 +16,19 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     @Query(value = "select * from Department", nativeQuery = true)
     public List<Request> getAllDepartmnet();
+    
+    @Modifying(clearAutomatically = true)
 
     @Query("select new com.college.core.entity.Department(dept.departmentHod) from Department dept where dept.departmentId = :deptId")
     public Department getHodName(@Param("deptId") Long deptId);
-
+    
+    @Transactional
+    @Modifying
     @Query(value = "update Department set departmentHod = :facultyName where departmentId = :departmentId")
-    public Department updateHodName(@Param("facultyName") String facultyName, @Param("departmentId") Long departmentId);
+    public void  updateHodName(@Param("facultyName") String facultyName, @Param("departmentId") Long departmentId);
+    
+    @Transactional
+    @Modifying
+    @Query(value="update Department set departmentHod = NULL where departmentId = :departmentId")
+    public void deleteHodName(@Param("departmentId") Long departmentId);
 }

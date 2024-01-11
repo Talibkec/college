1b4921e70a88d4cd3ce0868422fded7cf53c6856
keccc <vishalkrsinghmsk@gmail.com>
2024-01-11 14:20:04 +0530

    aws removal import

--- src/main/java/com/college/core/controller/AcademicController.java
+++ src/main/java/com/college/core/controller/AcademicController.java
@@ -11,8 +11,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.jws.Oneway;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
